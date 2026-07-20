c56f7580011f4b37a50c815aee7e753ae02e9720
fitiarakotoarison38 <fitiarakotoarison38@gmail.com>
2026-07-20 13:28:46 +0300

    css

--- app/Views/accueil.php
+++ app/Views/accueil.php
@@ -11,8 +11,8 @@
 
 
 <link 
-href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
-rel="stylesheet">
+ rel="stylesheet" href="<?= base_url('assets/bootstrap/css/bootstrap.min.css') ?>">
+<script src="<?= base_url('assets/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
 
 
 </head>
