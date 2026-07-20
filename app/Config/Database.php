ff5c608e55daca0daa473f9df293e047f626aef8
fitiarakotoarison38 <fitiarakotoarison38@gmail.com>
2026-07-20 09:19:52 +0300

    Base

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -26,11 +26,11 @@ class Database extends Config
      */
     public array $default = [
         'DSN'          => '',
-        'hostname'     => 'localhost',
+        'hostname'     => '',
         'username'     => '',
         'password'     => '',
-        'database'     => '',
-        'DBDriver'     => 'MySQLi',
+        'database'     => WRITEPATH . 'database/base.db',
+        'DBDriver'     => 'SQLite3',
         'DBPrefix'     => '',
         'pConnect'     => false,
         'DBDebug'      => true,
