a5c97fa92331676552cb2518f3aa008e4dabeed2
Aristide3182 <aristide3182@gmail.com>
2026-07-21 09:47:06 +0300

    Transformation dashboard pro

--- app/Models/ClientModel.php
+++ app/Models/ClientModel.php
@@ -102,7 +102,7 @@ class ClientModel extends Model
     }
 
     /**
-     * Fonction(dépôt, ou réception de transfert).
+     * Fonction (dépôt, ou réception de transfert).
      */
     public function crediter(int $idClient, float $montant): bool
     {
